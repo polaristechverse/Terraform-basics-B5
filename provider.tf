d6e8560b7e298bf88ddbdf76f476b653932a6df7
chaitanya <chaitanya@gmail.com>
2026-02-09 19:06:37 +0530

    added state backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket  = "cloudgroupbackend"
+    key     = "cginfra/basicinfra.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
---
+++ s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "DemoCGS3Bucket" {
+  bucket = "cloudgroupbackend"
+}
